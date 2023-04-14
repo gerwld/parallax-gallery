d71ec92db0b0ba6e86d03a592d5007b18913064c
P Jaworski <pjaworski.dev@gmail.com>
2023-04-14 16:42:26 +0200

    Added scroll functional with animation

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+const gallery = document.getElementById('gallery-container');
+
+const handleOnDown = (e) => {
+  gallery.dataset.mouseDownAt = e.clientX;
+}
+
+const handleOnUp = (e) => {
+  gallery.dataset.prevPercentage = gallery.dataset.percentage;
+  gallery.dataset.mouseDownAt = "0";
+}
+
+const handleOnMove = (e) => {
+  if(gallery.dataset.mouseDownAt === "0") return ;
+  const mouseDelta = parseFloat(gallery.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentage = Math.max(Math.min(parseFloat(gallery.dataset.prevPercentage) + percentage, 0), -100);
+
+  gallery.dataset.percentage = nextPercentage;
+
+  gallery.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, {duration: 1400, fill: "forwards"})
+
+
+  for(let image of gallery.getElementsByClassName('image')) {
+    image.animate({
+      objectPosition: `${nextPercentage + 100}% 50%`
+    }, {duration: 1400, fill: "forwards"})
+  }
+}
+
+window.onmousedown = handleOnDown;
+window.onmousemove = handleOnMove;
+window.onmouseup = handleOnUp;
+
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+window.ontouchend = e => handleOnUp(e.touches[0]);
